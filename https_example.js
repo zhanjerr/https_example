43c0ff05c3901d327eb6703b784da44143bc2a04
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-10 02:49:09 +0000

    switched to bigger response with many chunks

--- https_example.js
+++ https_example.js
@@ -3,13 +3,17 @@ var https = require("https");
 // console.log("I did it!");
 
 var options ={
-  host: "www.example.org",
-  path: "/"
+  host: "stream-large-file.herokuapp.com",
+  path: "/give-me-stuff-now"
 }
 
 var callback = function (response) {
   console.log("In response handler callback!");
-  console.log("Response: ", response);
+  // console.log("Response: ", response);
+  response.on("data", function(chunk){
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  })
 }
 
 console.log("I'm about to make the request!");
