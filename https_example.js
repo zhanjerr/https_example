a418f58df712ee1c356f8ebac7af2f313a53bb15
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-10 02:35:13 +0000

    tested https request on a small page

--- https_example.js
+++ https_example.js
@@ -1,3 +1,18 @@
 var https = require("https");
 
-console.log("I did it!");
+// console.log("I did it!");
+
+var options ={
+  host: "www.example.org",
+  path: "/"
+}
+
+var callback = function () {
+  console.log("In response handler callback!");
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
