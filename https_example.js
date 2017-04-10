591433ba996562618836758b71c19a15632338a4
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-10 02:40:16 +0000

    logged response

--- https_example.js
+++ https_example.js
@@ -7,8 +7,9 @@ var options ={
   path: "/"
 }
 
-var callback = function () {
+var callback = function (response) {
   console.log("In response handler callback!");
+  console.log("Response: ", response);
 }
 
 console.log("I'm about to make the request!");
